bd9c6cf28dd7f176f8da9ff22964654fc2d01e51
## <##>
2025-04-14 11:03:38 +0530

    Initial project commit

---
+++ src/main/java/learn/springframework/myLearning_pet_clinic/MyLearningPetClinicApplication.java
@@ -0,0 +1,13 @@
+package learn.springframework.myLearning_pet_clinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyLearningPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyLearningPetClinicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/learn/springframework/myLearning_pet_clinic/MyLearningPetClinicApplicationTests.java
@@ -0,0 +1,16 @@
+package learn.springframework.myLearning_pet_clinic;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+class MyLearningPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
